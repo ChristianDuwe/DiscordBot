609a38d72180ab753c00565ee6935d098d113374
Duwe <78703726+DuweBTC@users.noreply.github.com>
2025-04-20 17:23:18 +0200

    fixed minor spelling errors

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main()
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
     let intents = GatewayIntents::GUILD_MESSAGES
-    | GatewayIntents::GUILD_MESSAGES
+    | GatewayIntents::DIRECT_MESSAGES
     | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
