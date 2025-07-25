5621fbbc2f27110c6d4a08d17c03e0a7936d3f83
Duwe <78703726+DuweBTC@users.noreply.github.com>
2025-07-25 17:47:14 +0200

    Improved reaction handling

--- src/role_assignment/reaction_handler.rs
+++ src/role_assignment/reaction_handler.rs
@@ -7,52 +7,82 @@ pub struct ReactionHandler;
 
 impl ReactionHandler{ 
     pub async fn reaction_add( ctx: Context, reaction: Reaction) {
-        println!("Handling reaction: {:?}", reaction);
-
-        if reaction.message_id != 1366013962801647636
+        if reaction.message_id != 1398319986111549586
         {
             return;
         }
 
-        if let (Some(guild_id), Some(user_id)) = (reaction.guild_id, reaction.user_id) {
-            let emoji_str = reaction.emoji.unicode_eq("👍");
+        if let (Some(guild_id), Some(user_id)) = (reaction.guild_id, reaction.user_id) 
+        {
+            let (custom_id, custom_name) = match &reaction.emoji
+            { 
+                ReactionType::Custom {id, name, .. } => (Some(id.get()), name.as_deref()),
+                _ => (None, None),
+            };
 
-            if emoji_str == true
+            match (custom_id, custom_name)
             {
-                println!("Assigning role: {:?}", emoji_str);
-                let role_id = 1366013701165027408;
-
-                let member = guild_id.member(&ctx.http, user_id).await.unwrap();
-                println!("{:?}", member);
-
-                let add = member.add_role(&ctx.http, role_id).await;
-                println!("Role added {:?}", add);
-            }
+                (Some(1195363888254955682), Some("LWKekW")) => 
+                {
+                    Self::add_role(ctx, guild_id, user_id, 1087414816068620358.into()).await;
+                }
+                (Some(1105174564390060052), Some("pepeLove")) => 
+                {
+                    Self::add_role(ctx, guild_id, user_id, 1087414434441461840.into()).await;
+                }
+                _ => {
+                    println!("No match");
+                }
+            }   
         }
     }
 
     pub async fn reaction_remove( ctx: Context, reaction: Reaction) {
-        if reaction.message_id != 1366013962801647636
+        if reaction.message_id != 1398319986111549586
         {
             return;
         }
 
         if let (Some(guild_id), Some(user_id)) = (reaction.guild_id, reaction.user_id)
-        {
-            let emoji_str = reaction.emoji.unicode_eq("👍");
+        { 
+            let (custom_id, custom_name) = match &reaction.emoji
+            { 
+                ReactionType::Custom {id, name, .. } => (Some(id.get()), name.as_deref()),
+                _ => (None, None),
+            };
 
-            if emoji_str == true
-            {
-                println!("Removing role: {:?}", emoji_str);
-                let role_id = 1366013701165027408u64;
-
-                let member = guild_id.member(&ctx.http, user_id).await.unwrap();
-
-                let remove = member.remove_role(&ctx.http, role_id).await;
-                println!("Role removed: {:?}", remove);
+            match (custom_id, custom_name)
+            { 
+                (Some(1195363888254955682), Some("LWKekW")) => 
+                {
+                    Self::remove_role(ctx, guild_id, user_id, 1087414816068620358.into()).await;
+                }
+                (Some(1105174564390060052), Some("pepeLove")) => 
+                {
+                    Self::remove_role(ctx, guild_id, user_id, 1087414434441461840.into()).await;
+                }
+                _ => {
+                    println!("No match");
+                }
             }
-
         }
+    }
 
+    async fn add_role(ctx: Context, guild_id: GuildId, user_id: UserId, role_id: RoleId) 
+    {
+        let member = guild_id.member(&ctx.http, user_id).await.unwrap();
+        println!("{:?}", member);
+
+        let add = member.add_role(&ctx.http, role_id).await;
+        println!("Role added {:?}", add);
+    }
+
+    async fn remove_role(ctx: Context, guild_id: GuildId, user_id: UserId, role_id: RoleId) 
+    { 
+        let member = guild_id.member(&ctx.http, user_id).await.unwrap();
+        println!("{:?}", member);
+
+        let remove = member.remove_role(&ctx.http, role_id).await;
+        println!("Role removed {:?}", remove);
     }
 }
